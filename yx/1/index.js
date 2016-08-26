184ab32cc717ba1f0e6e08cf2c63b17788d0a52c
杨叙 <yx53074@hotmail.com>
2016-08-26 14:53:01 +0800

    111

---
+++ yx/1/index.js
@@ -0,0 +1,113 @@
+
+//import {testDemo} from './test';
+
+// var test=[
+//     {
+//         name:'ttaccp1',
+//         age:'102',
+//         height:110,
+//         width:110
+//     },
+//     {
+//         name:'ttaccp1',
+//         age:'102',
+//         height:110,
+//         width:110
+//     },
+//     {
+//     name:'ttaccp3',
+//     age:'102',
+//     height:110,
+//     width:110
+//     }
+// ]
+// var app=document.querySelector("#app");
+// var str=`
+//         <ul>
+//             <% for (var i=0,len=test.length;i<len;i++){%>
+//                 <li><%= test[i].name %></li>    
+//             <%}%>    
+//         </ul>
+//     `;
+// app.innerHTML=str;
+
+// var x=1;
+// var y=2;
+// app.innerHTML+=`${x+y}`;
+// var s1=Math.pow(2,53)-1;
+// var s2=Number.isSafeInteger(s1);
+// console.log(s2)
+
+// var s1=0.995;
+// var s2=Math.round(s1*100);
+// console.log(Number.parseInt(s2));
+// var c=function(c){
+//     return console.log(c);
+// };
+// c("11");
+// function test1(a,b){
+//     return Array.from(arguments);
+// }
+// console.log(test1(1,2));
+// console.log(Array.from('test'))
+
+
+// Object.defineProperties(proxy,'name',{
+//     value:'332',
+//     writeable:false
+// });
+
+// let letter=['a','b','c'];
+// let entries=letter.entries();
+// console.log(entries.next().value);
+// console.log(entries.next().value);
+// console.log(entries.next().value);
+// console.log(entries.next().value);
+
+// let a1=[1,2,3,4,5];
+
+// console.log(a1)
+// let a2=a1.map((item)=>{return item*item})
+// console.log(a2)
+
+// function demo(x,y="sdsds"){
+//     console.log(x+y);
+// }
+// demo('张三','dsdsddddd')
+// let foo='dsds';
+// let f= x =>foo;
+// var f=function(x){
+//     return foo;
+// }
+// function f(x){
+//     return foo
+// }
+// console.log(f())
+
+// const sortNum=(...number)=>{
+//     return number.sort();
+// }
+
+// console.log(...[1,2,6,8,1,3,3,4])
+
+// function test(a,b,c){
+//     var s= [...arguments];
+//     console.log(typeof s);
+//     console.log(s);
+// }
+// test(1,4,6,7,3)
+
+// console.log(Math.max(...[4,6,7,9,23,45,1,22]));
+// function test(a,b,c){
+//     return [...arguments];
+// }
+// console.log(test(1,4,6,7,3));
+// console.log([...'hello'])
+
+
+// console.log([...'hello'].length)
+
+
+
+
+
---
+++ yx/1/test.js
@@ -0,0 +1,2 @@
+var name="age"
+export {name};
